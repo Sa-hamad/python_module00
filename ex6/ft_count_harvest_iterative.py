3951b6f6499a676fca77aef2ecf5d0c8ba321d7b
Sara Hamad <sara.hamad555@gmail.com>
2026-05-02 18:59:03 +0400

    save

---
+++ ex6/ft_count_harvest_iterative.py
@@ -0,0 +1,8 @@
+
+def ft_count_harvest_iterative()
+i = 1
+days = input ("Days until harvest: ")
+while (i <= int(days)):
+	print ("Day", int(i))
+	i += 1
+print ("Harvest time!")
---
+++ ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,5 @@
+i = 1
+days = input ("Days until harvest: ")
+print ("Day", range(1, (int(days))))
+
+print ("Harvest time!")
